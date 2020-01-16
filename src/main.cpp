ff1af1cde720f657dfcdc1b5a4bc65b5e3faba84
eddiecarbin <paullaster@yahoo.com>
2020-01-16 07:06:06 -0800

    complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,109 @@
 #include <Arduino.h>
 #include "SoftwareSerial.h"
 #include <JC_Button.h>
-//#include "../lib/SoundPlayer/SoundPlayer.h"
+#include "../lib/SoundPlayer/SoundPlayer.h"
+#include "../lib/FSM/FSM.h"
 #include "DFRobotDFPlayerMini.h"
 
 #define LAUNCH_BUTTON_PIN 7
 #define COUNTDOWN_BUTTON_PIN 5
 
+#define GO_TO_COUNTDOWN_STATE 1
+#define GO_TO_LAUNCH_STATE 2
+
 Button launchButton(LAUNCH_BUTTON_PIN);
 
 Button countdownButton(COUNTDOWN_BUTTON_PIN);
-//SoundPlayer soundPlayer(18, 15);
-SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
+SoundPlayer soundPlayer(18, 15);
+
+State StateDoNothing(NULL, NULL, NULL);
+Fsm fsm(&StateDoNothing);
+
+bool _soundIsPlaying = false;
+
+void OnCountDownStateEnter()
+{
+}
+
+void OnCountDownStateUpdate()
+{
+
+  if (!_soundIsPlaying)
+  {
+    if (launchButton.wasReleased()) // if the button was released, change the LED state
+    {
+      Serial.println("Do Nothing Cant press button");
+    }
+
+    if (countdownButton.wasReleased()) // if the button was released, change the LED state
+    {
+      Serial.println("countdown button pressed");
+      _soundIsPlaying = true;
+      soundPlayer.PlaySound(1);
+    }
+  }
+  else if (soundPlayer.isPlaying() == false)
+  {
+    /// switcht o other state
+    _soundIsPlaying = false;
+    fsm.trigger(GO_TO_LAUNCH_STATE);
+  }
+}
+
+void OnLaunchStateEnter()
+{
+}
+
+void OnLaunchStateUpdate()
+{
+  if (!_soundIsPlaying)
+  {
+    if (launchButton.wasReleased()) // if the button was released, change the LED state
+    {
+      Serial.println(" button");
+      _soundIsPlaying = true;
+
+      soundPlayer.PlaySound(2);
+    }
+    if (countdownButton.wasReleased()) // if the button was released, change the LED state
+    {
+      Serial.println("countdown button pressed");
+    }
+  }
+  else if (soundPlayer.isPlaying() == false)
+  {
+    /// switcht o other state
+    _soundIsPlaying = false;
+    fsm.trigger(GO_TO_COUNTDOWN_STATE);
+  }
+}
+
+State CountDownState(&OnCountDownStateEnter, &OnCountDownStateUpdate, NULL);
+State LaunchState(&OnLaunchStateEnter, &OnLaunchStateUpdate, NULL);
 
 void setup()
 {
   Serial.begin(9600);
   // put your setup code here, to run once:
   delay(1000);
-
   launchButton.begin();
   countdownButton.begin();
-  mySoftwareSerial.begin(9600);
+  soundPlayer.initialize();
+  //soundPlayer.PlaySound(1);
 
-  // soundPlayer.initialize();
-  if (!myDFPlayer.begin(mySoftwareSerial))
-  { //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while (true)
-      ;
-  }
-  return;
-  //Serial.println(F("DFPlayer Mini online."));
+  fsm.add_transition(&CountDownState, &LaunchState,
+                     GO_TO_LAUNCH_STATE, NULL);
+  fsm.add_transition(&LaunchState, &CountDownState,
+                     GO_TO_COUNTDOWN_STATE, NULL);
 
-  //myDFPlayer.volume(30); //Set volume value. From 0 to 30
-
-  //int fileCount = myDFPlayer.readFileCounts();
-  // myDFPlayer.play(1); //Play the first mp3
-
-  //Serial.print("file count = ");
-  // Serial.println(fileCount);
+  fsm.goToState(&CountDownState);
 }
 
 void loop()
 {
-
   launchButton.read();
   countdownButton.read();
-  //soundPlayer.update();
-
-  if (launchButton.wasReleased()) // if the button was released, change the LED state
-  {
-    Serial.println("launche button pressed");
-    //soundPlayer.PlaySound(2);
-  }
-  if (countdownButton.wasReleased()) // if the button was released, change the LED state
-  {
-    Serial.println("countdown button pressed");
-
-    //soundPlayer.PlaySound(1);
-  }
-
-  Serial.println("test");
-  // put your main code here, to run repeatedly:
+  soundPlayer.update();
+  fsm.run_machine();
+  delay(100);
 }
